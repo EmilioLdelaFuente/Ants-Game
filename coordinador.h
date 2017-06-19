bc5bef5b02806218891cd9414792d1fe6809d272
EmilioLdelaFuente <emilio.ldlf@gmail.com>
2017-06-20 01:08:53 +0200

    coordinador no acabado
    queda añadirle un apr de cosas, además es una versión muy basica

---
+++ coordinador.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Mapa.h"
+#include "ETSIDI.h"
+class coordinador
+{
+public:
+	coordinador();
+	virtual ~coordinador();
+
+	void teclaEspecial(unsigned char key);
+	void tecla(unsigned char key);
+	void mueve();
+	void dibuja();
+protected:
+	Mapa mapa;
+	enum Estado { INICIO, JUEGO, GAMEOVER, FIN};
+	Estado estado;
+};
+

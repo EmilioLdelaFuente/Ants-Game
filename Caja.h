f57bee888b7ffbb9bd68f531d4453042a6de5698
LuisdeBenito <luis.debenito2010@gmail.com>
2017-06-19 17:21:36 +0200

    Add files via upload

---
+++ Caja.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Pared.h"
+class Caja
+{
+public:
+	Caja();
+	virtual ~Caja();
+	void dibuja();
+	friend class Interaccion;
+private:
+	Pared suelo;
+	Pared techo;
+	Pared pared_izq;
+	Pared pared_dcha;
+
+};
+
